2fbfbbddbc62982da3ce1e99c7f5e200970f6555
Reshad <rmajidi09@gmail.com>
2021-07-19 12:55:51 +0200

    Finished week 4 exercises and tested

--- 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
+++ 1-JavaScript/Week4/homework/ex3-lemonAllergy.test.js
@@ -24,26 +24,30 @@ const fruitBasket = [
 ];
 
 // ! Function under test
-function sanitizeFruitBasket(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function sanitizeFruitBasket(fruitBasketArray, nameOfFruit) {
+  const filteredFruitBasket = fruitBasketArray.filter((item) => {
+    return item === nameOfFruit;
+  });
+  return filteredFruitBasket;
 }
 
 // ! Unit tests (using Jest)
 describe('sanitizeFruitBasket', () => {
   test('should take two parameters', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    expect(sanitizeFruitBasket).toHaveLength(2);
   });
 
   test('should not modify the original `fruitBasket` array', () => {
     // Save the original contents of the fruit basket
     const originalFruitBasketContents = [...fruitBasket];
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    const result = originalFruitBasketContents.length === fruitBasket.length;
+    const expected = true;
+    expect(result).toBe(expected);
   });
 
   test('should return a new array that does not include the unwanted `lemon`', () => {
-    // TODO replace next line with your code
-    expect(false).toBe(true);
+    const result = sanitizeFruitBasket(fruitBasket).includes('lemon');
+    const expected = false;
+    expect(result).toBe(expected);
   });
 });
